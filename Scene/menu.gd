5b44cb3f47460b0b77d4a750a6b76964d5609e5f
shivenmags <sarora@students.mags.school.nz>
2024-09-06 12:29:03 +1200

    added main menu

---
+++ Scene/menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var play_button: Button = $MainMenu1/PlayButton
+@onready var exit_button: Button = $MainMenu1/ExitButton
+
+func _ready() -> void:
+	# Connect button signals
+	play_button.connect("pressed", Callable(self, "_on_play_button_pressed"))
+	exit_button.connect("pressed", Callable(self, "_on_exit_button_pressed"))
+
+func _on_play_button_pressed() -> void:
+	# Load the game scene
+	get_tree().change_scene_to_file("res://level.tscn")  # Change this to your actual game scene path
+
+func _on_exit_button_pressed() -> void:
+	# Exit the game
+	get_tree().quit()
