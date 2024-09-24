ee9b5b6fc2fd62f9acc680e5ff19a978edefa3ed
shivenmags <sarora@students.mags.school.nz>
2024-09-24 20:18:02 +1200

    added acceleration

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,38 +1,45 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var current_score = 0
+var current_speed = 0.0  
+var acceleration = 200.0 
+var deceleration = 150.0  
+var max_speed = 5500.0  
+var min_speed = 50.0  
+var current_score = 0  
 
-@export var score_ui : Node
-
+@export var score_ui : Node  
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-
-	# Handle jump.
-	velocity.y = -550.0
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if Input.is_action_pressed("ui_up"):
+		
+		current_speed += acceleration * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		current_speed -= deceleration * delta
+	
+	
+	current_speed = clamp(current_speed, min_speed, max_speed)
 
+	
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	
+	velocity.x = direction * current_speed
+
+	
+	velocity.y = current_speed * -1  
+
+	
 	move_and_slide()
-
 
 func crash(body: Node2D) -> void:
 	if body.has_meta("car"):
-		get_tree().change_scene_to_file("res://Scene/menu.tscn") # Replace with function body.
-
-
+		get_tree().change_scene_to_file("res://Scene/menu.tscn")
 
 func Score(body: Node2D) -> void:
 	if body.has_meta("car"):
-		current_score = current_score + 1 # Replace with function body.
+		current_score += 1  
 		print(current_score)
 		score_ui.text = "Score: " + str(current_score)
